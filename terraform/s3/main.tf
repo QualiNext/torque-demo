aa10178a19ef396f8b9e6f3dcbb63289f126481b
Tomer Admon <tomer.a@qualisystems.com>
2022-08-22 11:09:28 +0300

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -30,7 +30,6 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-# comment number somethign else
 resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
