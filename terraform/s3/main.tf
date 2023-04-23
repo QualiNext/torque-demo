ffe351fe037817ae1c1a9d4cf8175d965539a857
David B <96681520+davidbquali@users.noreply.github.com>
2023-04-23 09:09:44 +0300

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -32,10 +32,30 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
+resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.bucket.id
-  acl = "public-read"
 
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {                                                                   
+    bucket = aws_s3_bucket.bucket.id
+    rule {
+      object_ownership = "BucketOwnerPreferred"
+    } 
+  }
+
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [aws_s3_bucket_public_access_block.example,
+                aws_s3_bucket_ownership_controls.example,
+  ]
+  bucket = aws_s3_bucket.bucket.id
+
+  acl = "public-read"
 }
 
 #my comment
