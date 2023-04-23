98f8f64611b6087cd8a542f89dfb8fbd8c821cd0
David B <96681520+davidbquali@users.noreply.github.com>
2023-04-23 09:01:18 +0300

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -34,7 +34,8 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
+  acl = "public-read"
+
 }
 
 #my comment
